6f9186b96a7577e33fc0ebb2625d1b3dd080f007
종현 이 <dlwhdrms999@naver.com>
2020-10-11 18:37:56 +0900

    Github PUSH 예시

---
+++ Github_Test/Github_Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            int result = a + b;
+
+            Console.WriteLine(result);
+
+            Console.Read();
+        }
+    }
+}
